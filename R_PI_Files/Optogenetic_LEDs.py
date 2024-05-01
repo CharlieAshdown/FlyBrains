8a21acddbd00e510ae8addf1c5b2e3ee2015c2eb
CharlieAshdown <charlesdavidashdown@gmail.com>
2024-05-01 09:44:36 +0100

    Updated raspberry pi files

--- R_PI_Files/Optogenetic_LEDs.py
+++ R_PI_Files/Optogenetic_LEDs.py
@@ -1,35 +1,36 @@
 import RPi.GPIO as GPIO
 import yaml
-from time import time as t
+import time
 
 from time import sleep
 
+start = time.time()
 try:
     with open('/media/charlie/FLYBRAINS5/config.yaml', 'r') as file:
         configs = yaml.safe_load(file)
-    root = "/media/charlie/FLYBRAINS5/samples/"
+    root = "/media/charlie/FLYBRAINS5/samples/test_"+str(configs["test_number"])+"/"
 except:
     with open('config.yaml', 'r') as file:
         configs = yaml.safe_load(file)
-    root = "samples"
+    root = "samples/test_"+str(configs["test_number"])
     
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(24,GPIO.OUT)
-test_number = configs["test_number"]
-test_file = f"{root}/{test_number}"
+
 
 Optogenetics_led = GPIO.PWM(24,configs["Optogenetic_LEDs"]["frequency"])
 Optogenetics_led.start(0)
 sleep(configs["Optogenetic_LEDs"]["initial_delay"])
-leds_on = t()
+LED_ON = time.time()
 Optogenetics_led.ChangeDutyCycle(configs["Optogenetic_LEDs"]["duty"])
 sleep(configs["Optogenetic_LEDs"]["flash_length"])
+LED_OFF = time.time()
 Optogenetics_led.ChangeDutyCycle(0)
-leds_off = t()
 GPIO.cleanup()
 
-with open(f"{test_file}/test_{test_number}_LED_timings", "w") as file:
-    file.write(f"LEDs ON: {leds_on}\tLEDs OFF: {leds_off}")
+f = open(root+"_LED_timings.txt", "w")
+f.write(f"LEDs ON: {LED_ON-start} \t LEDs OFF: {LED_OFF-start}")
+f.close()
